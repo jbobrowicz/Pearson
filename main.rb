06dff408d3ade6131e78d698cc9aeb9e36ba9777
Jakub Bobrowicz <jbobrowicz@gmail.com>
2012-01-17 21:35:10 +0100

    Revert fc779fb07eaeb40084c48e00e3393e695a2f4bee^..HEAD

--- main.rb
+++ main.rb
@@ -3,7 +3,7 @@ require_relative "lib/cfg"
 
 begin
   cfg = Cfg.new
-  planner = Planner.new(cfg)
+  planner = Planner.new(cfg.argv, cfg.input, cfg.output)
   planner.run
 rescue => e
   $stdout.puts e
